14d2788bf2a76f535d5f035da955af511c4dde34
Samuel José Candido <101359102+SamuelCandido@users.noreply.github.com>
2023-11-30 12:11:55 +0000

    fix: gitgnore
    add new ignore item
    
    close

--- run/server_back.py
+++ run/server_back.py
@@ -1,6 +1,8 @@
 from back.model.import_model import *
 from back.config.config import *
 from back.config.config import path
+from back.login.login import *
+from back.login.singup import *
 
 @app.route("/")
 def start():
